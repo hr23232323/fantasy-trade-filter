d11b53f9192793c05c5607c43a470507fbea0b84
Harsh Rana <hrana@wpi.edu>
2024-11-16 13:50:04 -0500

    More colors

--- frontend/src/app/components/PlayerTableRow.tsx
+++ frontend/src/app/components/PlayerTableRow.tsx
@@ -10,6 +10,29 @@ interface PlayerTableRowProps {
   toggleExpandRow: (slug: string) => void;
 }
 
+const getPositionRowColor = (position: string) => {
+  switch (position) {
+    case "QB":
+      return "bg-blue-100"; // Subtle blue for QB
+    case "WR":
+      return "bg-orange-100"; // Subtle orange for WR
+    case "RB":
+      return "bg-green-100"; // Subtle green for RB
+    case "TE":
+      return "bg-purple-100"; // Subtle purple for TE
+    default:
+      return "bg-gray-100"; // Neutral gray for others
+  }
+};
+
+const getValueBadgeStyle = (tier: number) => {
+  if (tier <= 3)
+    return "bg-green-100 text-green-700 font-semibold rounded-full px-2 py-1";
+  if (tier > 3)
+    return "bg-yellow-100 text-yellow-700 font-semibold rounded-full px-2 py-1";
+  return "bg-red-100 text-red-700 font-semibold rounded-full px-2 py-1";
+};
+
 export const PlayerTableRow: FC<PlayerTableRowProps> = ({
   player,
   isExpanded,
@@ -17,7 +40,11 @@ export const PlayerTableRow: FC<PlayerTableRowProps> = ({
 }) => {
   return (
     <Fragment key={player.slug}>
-      <tr className="border-t hover:bg-gray-50">
+      <tr
+        className={`border-t hover:bg-gray-100 ${getPositionRowColor(
+          player.position
+        )}`}
+      >
         <td className="p-3 flex items-center">
           {player.playerName}
           {<InjuryIndicator player={player} />}
@@ -25,8 +52,22 @@ export const PlayerTableRow: FC<PlayerTableRowProps> = ({
         <td className="p-3">{player.position}</td>
         <td className="p-3">{player.age}</td>
         <td className="p-3">{player.teamLongName}</td>
-        <td className="p-3">{player.oneQBValues.value}</td>
-        <td className="p-3">{player.superflexValues.value}</td>
+        <td className="p-3">
+          <span
+            className={getValueBadgeStyle(player.oneQBValues.positionalTier)}
+          >
+            {player.oneQBValues.value}
+          </span>
+        </td>
+        <td className="p-3">
+          <span
+            className={getValueBadgeStyle(
+              player.superflexValues.positionalTier
+            )}
+          >
+            {player.superflexValues.value}
+          </span>
+        </td>
         <td className="p-3 text-right">
           <button
             onClick={() => toggleExpandRow(player.slug)}
