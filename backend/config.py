526705feff1728af6f593c1294ae26deb625d095
Harsh Rana <hrana@wpi.edu>
2024-11-16 19:37:07 -0500

    Some meta changes

--- backend/config.py
+++ backend/config.py
@@ -13,8 +13,7 @@ origins = [
 def add_cors_middleware(app):
     app.add_middleware(
         CORSMiddleware,
-        # allow_origins=origins,  # Allows all listed origins
-        allow_origins=["*"],
+        allow_origins=origins,
         allow_credentials=True,
         allow_methods=["*"],  # Allows all HTTP methods (GET, POST, etc.)
         allow_headers=["*"],  # Allows all headers
