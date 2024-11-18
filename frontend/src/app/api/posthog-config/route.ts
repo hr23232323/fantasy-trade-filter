19b0c4b39249c3a11b6f9367965b0181bb76857a
Harsh Rana <hrana@wpi.edu>
2024-11-18 08:05:03 -0500

    trying to make posthog work

--- frontend/src/app/api/posthog-config/route.ts
+++ frontend/src/app/api/posthog-config/route.ts
@@ -1,15 +1,19 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(request: NextRequest): Promise<NextResponse> {
-  try {
-    // Fetch runtime environment variables from process.env
-    const runtimeEnv = {
-      NEXT_PUBLIC_ENABLE_POSTHOG: process.env.NEXT_PUBLIC_ENABLE_POSTHOG || "0",
-      NEXT_PUBLIC_POSTHOG_KEY: process.env.NEXT_PUBLIC_POSTHOG_KEY || "",
-      NEXT_PUBLIC_POSTHOG_HOST: process.env.NEXT_PUBLIC_POSTHOG_HOST || "",
-    };
 
-    return NextResponse.json(runtimeEnv, { status: 200 });
+  try {
+    const posthog_enabled = process.env.NEXT_PUBLIC_ENABLE_POSTHOG;
+    const posthog_key = process.env.NEXT_PUBLIC_POSTHOG_KEY;
+    const posthog_url = process.env.NEXT_PUBLIC_POSTHOG_HOST;
+
+    const vars = {
+      NEXT_PUBLIC_ENABLE_POSTHOG: posthog_enabled,
+      NEXT_PUBLIC_POSTHOG_KEY: posthog_key,
+      NEXT_PUBLIC_POSTHOG_HOST: posthog_url,
+    }
+
+    return NextResponse.json(vars, { status: 200 });
   } catch (error: any) {
     console.error('Error fetching runtime environment variables:', {
       message: error.message,
